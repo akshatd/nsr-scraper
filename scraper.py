75cd6afc5abdaca3807d1a6b761965652f5c28d2
Akshat Dubey <akshatdubey@outlook.com>
2020-03-13 03:29:06 +0800

    feat: parse personal info and climic info

--- scraper.py
+++ scraper.py
@@ -4,17 +4,17 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+import string
 
-URL_FILE_NAME = "urls.csv"
-
+URL_FILE_NAME = 'urls.csv'
 
 def loadUrlFile(url_file_name):
 	"""
 	Loads URLs from a file into a list
 	"""
 	urls = []
-	with open("urls.csv") as urls_file:
-		for url in urls_file:
+	with open(url_file_name) as url_file:
+		for url in url_file:
 			urls.append(url.strip())
 	return urls
 
@@ -33,13 +33,53 @@ def parseDoctorUrl(url):
 	"""
 	Parses a single URL that points to a doctor's NSR webpage
 	"""
-	print(url)
 	raw_html = simple_get(url)
 	if raw_html is not None:
 		html = BeautifulSoup(raw_html, features="html.parser")
-		return html
+		main_table = html.find('table', attrs={'id':'tableMain'})
+		doctor_info = {}
+		personal_data = parsePersonalData(main_table)
+		doctor_info.update(personal_data)
+		clinical_practise = parseClinicalPractise(main_table)
+		doctor_info.update(clinical_practise)
+		return doctor_info
 	else:
 		return None
+
+def parsePersonalData(main_table):
+	personal_data_table = main_table.find('th', text='Personal Data').parent
+	personal_data = {}
+	for table_row in personal_data_table.find_next_siblings():
+		data_list = table_row.find_all('td')
+		if len(data_list) == 3:
+			key = '(Personal) '+cleanUp(data_list[1].text)
+			personal_data[key] = cleanUp(data_list[2].text)
+	return personal_data
+
+def parseClinicalPractise(main_table):
+	clinical_practise_table = main_table.find('td', text='Clinical Practice(s)').parent
+	clinical_practise = {}
+	clinical_practise_no = 1
+	for table_row in clinical_practise_table.find_next_siblings():
+		data_list = table_row.find_all('td')
+		if len(data_list) == 3:
+			key = '(Clinic '+str(clinical_practise_no)+') '+cleanUp(data_list[1].text)
+			if key in clinical_practise:
+				clinical_practise_no += 1
+				key = '(Clinic '+str(clinical_practise_no)+') '+cleanUp(data_list[1].text)
+			clinical_practise[key] = cleanUp(data_list[2].text)
+	return clinical_practise
+
+def cleanUp(text):
+	words = text.split(' ')
+	clean_words = []
+	for word in words:
+		clean_word = ''.join(char for char in word.strip() if (char in string.printable and char not in ['\r', '\n']))
+		if clean_word != '':
+			clean_words.append(clean_word)
+
+	clean_text = ' '.join(clean_words)
+	return clean_text
 
 def simple_get(url):
 	"""
