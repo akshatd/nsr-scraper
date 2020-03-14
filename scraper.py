76adfa990a09af285a2a8678aa5042d48aa7ff02
Akshat Dubey <akshatdubey@outlook.com>
2020-03-15 02:20:05 +0800

    feat: fully implemented qualifications parser

--- scraper.py
+++ scraper.py
@@ -9,6 +9,16 @@ from unicodedata import normalize
 
 URL_FILE_NAME = 'test.csv'
 
+# QUAL_ROWS = 5
+QUAL_COLUMNS = 3
+
+# Qualification ROWS
+QUAL_ROW_HEADING = 0
+QUAL_ROW_BASIC_HEADING = 1
+QUAL_ROW_BASIC_INFO = 2
+QUAL_ROW_SPECIALIST_HEADING = 3
+QUAL_ROW_SPECIALIST_INFO = 4
+
 def loadUrlFile(url_file_name):
 	"""
 	Loads URLs from a file into a list
@@ -76,20 +86,43 @@ def parseClinicalPractise(main_table):
 
 def parseQualifications(main_table):
 	qualifications_table = main_table.find('td', text='Qualifications').parent
+	# Qualifications' table is inside the heading's sibling, so get inside and find the rows
+	qualifications_rows = qualifications_table.find_next_siblings()[0].find_all('tr')
+	# Put rowwise data in the list
+	qualifications_list = []
+	for table_row in qualifications_rows:
+		qualifications_list.append(table_row.find_all('td'))
+
+	# check lengths of the important rows
+	if len(qualifications_list[QUAL_ROW_HEADING]) != 3 or \
+		len(qualifications_list[QUAL_ROW_BASIC_INFO]) !=3 or \
+		len(qualifications_list[QUAL_ROW_SPECIALIST_INFO]) != 3:
+		return {}
+
 	qualifications = {}
-	sibling_list = qualifications_table.find_next_siblings()
-	# sibling has just one table, so get inside and find the rows
-	for table_row in sibling_list[0].find_all('tr'):
-		data_list = table_row.find_all('td')
+	# get info for both the degrees
+	for column in range(QUAL_COLUMNS):
+		basic_key = ' '.join([qualifications_list[QUAL_ROW_HEADING][column].text, qualifications_list[QUAL_ROW_BASIC_HEADING][0].text])
+		basic_value = qualifications_list[QUAL_ROW_BASIC_INFO][column].text
+		qualifications[cleanUp(basic_key)] = cleanUp(basic_value)
+
+		specialist_key = ' '.join([qualifications_list[QUAL_ROW_HEADING][column].text, qualifications_list[QUAL_ROW_SPECIALIST_HEADING][0].text])
+		specialist_value = qualifications_list[QUAL_ROW_SPECIALIST_INFO][column].text
+		qualifications[cleanUp(specialist_key)] = cleanUp(specialist_value)
 
 	return qualifications
 
 def cleanUp(text):
+	# handle a case where & becomes &AMP, probably due to the HTML parser chosen for BeautifulSoup
+	text = text.replace('&AMP', '&')
+	# convert all random whitespace to spaces
+	for ch in string.whitespace:
+		text = text.replace(ch, ' ')
 	# normalise unicode to get rid of weird characters like \xa0
 	words = normalize("NFKD", text).split(' ')
 	clean_words = []
 	for word in words:
-		clean_word = ''.join(char for char in word.strip() if (char in string.printable and char not in ['\r', '\n']))
+		clean_word = ''.join(char for char in word.strip() if char in string.printable)
 		if clean_word != '':
 			clean_words.append(clean_word)
 
@@ -137,3 +170,4 @@ if __name__ == "__main__":
 	print("hello fuckers!!")
 	urls = loadUrlFile(URL_FILE_NAME)
 	doctor_info = getDoctorInfo(urls)
+	print(doctor_info)
